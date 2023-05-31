ebc10f3d9dc7b936d281f21ae7669868a1fa36b6
hannazo <106128732+hannazo@users.noreply.github.com>
2023-05-31 19:00:08 -0400

    added css to help page

--- client/src/pages/Help/Help.js
+++ client/src/pages/Help/Help.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Contact from './Contact';
 import FAQ from './FAQ';
+import './index.scss';
 
 function Help() {
     return (
-        <>
+        <div className="container help-page">
             <FAQ />
             <Contact />
-        </>
+        </div>
     )
 }
 
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -9,7 +9,7 @@ const Home = () => {
     <div className="container home-page">
       <div className="hero">
         <h1>Welcome!</h1>
-        <p>Happily Ever Prints is your one-stop destination for creatively designed and personalized printables. Whether you're planning an event, need custom stationery, or simply want to add a personal touch to your space, our mission is to help bring your ideas to life beautifully, making every moment special.</p>
+        <p>Happily Ever Prints is your one-stop destination for beautifully designed and personalized wedding printables. From save-the-dates and invitations to thank you cards and custom stationery, we aim to enhance the uniqueness of your special day. Our mission is to help bring your ideas to life beautifully, making every moment special.</p>
         <Link to="login" className="flat-button">GET STARTED</Link>
         <img src={Hero} className="hero-img" alt="Hero"></img>
       </div>
