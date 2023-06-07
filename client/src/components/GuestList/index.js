825596a7acfb23742c79a3a91a95cfa641d3b2b7
sinthushan <sinthushan@gmail.com>
2023-06-07 17:14:09 -0400

    fixing draggable elements

--- client/src/components/GuestList/index.js
+++ client/src/components/GuestList/index.js
@@ -16,30 +16,30 @@ function GuestList(props){
             alt='seating chart' 
             src='/images/White-Green-And-Black-Floral-Wedding-Seating-Chart-no-text.png' 
             className="card-image"/>
-            <span className="draggableField"  id="guestlistbride" style={{top: '21%', left: '23%',}}>{props.brideState}</span>
-            <span className="draggableField"  id="glampersand" style={{top: '21%', left: '48%',}}>&</span>
-            <span className="draggableField"  id="guestlistgroom" style={{top: '21%', left: '58%',}}>{props.groomState}</span>
-            <div id="table1" className="guestList table1 draggableField" style={{top: '38%', left: '7%',}}>
+            <span className="draggableField"  id="guestlistbride" style={{top: '21%', left: '23%',}} draggable="true">{props.brideState}</span>
+            <span className="draggableField"  id="glampersand" style={{top: '21%', left: '48%',}} draggable="true">&</span>
+            <span className="draggableField"  id="guestlistgroom" style={{top: '21%', left: '58%',}} draggable="true">{props.groomState}</span>
+            <div id="table1" className="guestList table1 draggableField" style={{top: '38%', left: '7%',}} draggable="true">
                 <h3>Table 1</h3>
                 <ul className="tableMembers" id="table1members"></ul>
             </div>
-            <div id="table2" className="guestList table2 draggableField" style={{top: '38%', left: '40%',}}>
+            <div id="table2" className="guestList table2 draggableField" style={{top: '38%', left: '40%',}} draggable="true">
                 <h3>Table 2</h3>
                 <ul className="tableMembers" id="table2members"></ul>
             </div>
-            <div id="table3" className="guestList table3 draggableField" style={{top: '38%', left: '71%',}}>
+            <div id="table3" className="guestList table3 draggableField" style={{top: '38%', left: '71%',}} draggable="true">
                 <h3>Table 3</h3>
                 <ul className="tableMembers" id="table3members"></ul>
             </div>
-            <div id="table4" className="guestList table4 draggableField" style={{top: '62%', left: '7%',}}>
+            <div id="table4" className="guestList table4 draggableField" style={{top: '62%', left: '7%',}} draggable="true">
                 <h3>Table 4</h3>
                 <ul className="tableMembers" id="table4members"></ul>
             </div>
-            <div id="table5" className="guestList table5 draggableField" style={{top: '62%', left: '40%',}}>
+            <div id="table5" className="guestList table5 draggableField" style={{top: '62%', left: '40%',}} draggable="true">
                 <h3>Table 5</h3>
                 <ul className="tableMembers" id="table5members"></ul>
             </div>
-            <div id="table6" className="guestList table6 draggableField" style={{top: '62%', left: '71%',}}>
+            <div id="table6" className="guestList table6 draggableField" style={{top: '62%', left: '71%',}} draggable="true">
                 <h3>Table 6</h3>
                 <ul className="tableMembers" id="table6members"></ul>
             </div>
