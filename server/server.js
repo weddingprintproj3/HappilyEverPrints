58b1431951b1a1a6ce273a1b79b7206ff0846b23
sinthushan <sinthushan@gmail.com>
2023-06-07 21:45:16 -0400

    Revert "changes to service worker"
    This reverts commit cb33ba7fec6bf8fde127aef96077a41943ed939d.

--- server/server.js
+++ server/server.js
@@ -33,8 +33,8 @@ app.post("/api/contact", (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-app.use('/images', express.static(path.join(__dirname, '../client/images')));
-app.get('/', (req, res) => {
+
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
